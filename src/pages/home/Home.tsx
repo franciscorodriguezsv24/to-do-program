a474edb581fcb3bc4f5be324ce9c3720b2d9b2a0
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-08-18 16:47:36 -0600

    feat: create layout base and added styles to dark mode

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import styles from './home.module.scss'
+
+export const Home = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.homeImgBackground}>
+      </div>
+      <div className={styles.homeBackground}>
+      </div>
+      <div className={styles.testingAbsolute}>
+        testing element
+      </div>
+    </div>
+  )
+}
