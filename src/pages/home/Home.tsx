b240e50961884ce4d6cfcfc0be339bf1ecadf983
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-08-19 12:00:46 -0600

    feat: add dark mode in the app

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,26 @@
 import styles from './home.module.scss'
+import Moon from '../../assets/icon-moon.svg'
+import Sun from '../../assets/icon-sun.svg'
+
+import { useTheme } from '../../context/ThemeModeContext' 
 
 export const Home = () => {
+
+  const { theme, toggleTheme} = useTheme()
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.homeImgBackground}>
       </div>
       <div className={styles.homeBackground}>
       </div>
-      <div className={styles.testingAbsolute}>
-        testing element
+      <div className={styles.toDoMainContainer}>
+        <div className={styles.toDoHeader}>
+        <h1 className={styles.toDoTitle}>TODO</h1>
+         <button onClick={toggleTheme}>
+          {theme === "light" ? <img src={Moon} alt='moon'/> : <img src={Sun} alt='sun'/>}
+        </button>
+        </div>
       </div>
     </div>
   )
